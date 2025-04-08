7c44d297fdab5e1d6325690bdb4014c867486572
Krushna.college <krushna.bhandarkar1908@gmail.com>
2025-04-08 16:29:09 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ISL_Project
+
+## Workflows
+1.constant
+2.entity
+3.components
+4.pipeline
+5.app.py
--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-from signLanguageDetection.logger import logging
-from signLanguageDetection.exception import AppException
-import sys
-
-try:
-    a = 3/"s"
-    
-except Exception as e:
-    raise AppException(e, sys)
+from signLanguageDetection.pipeline.training_pipeline import TrainPipeline
+obj = TrainPipeline()
+obj.run_pipeline()
